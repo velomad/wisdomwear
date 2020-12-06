4c058c96b63511e4aa3b720d720fdc106d8956e8
velomad <48247308+velomad@users.noreply.github.com>
2020-12-06 13:41:18 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const path = require("path")
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "..", "client-app/build")));
+	app.use(express.static(path.join(__dirname, "client-app/build")));
 	app.get("/*", (req, res) => {
-		res.sendFile(path.join(__dirname, "..", "client-app/build", "index.html"));
+		res.sendFile(path.join(__dirname, "client-app/build", "index.html"));
 	});
 }
 
