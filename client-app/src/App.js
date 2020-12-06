e356db246cc0a4b25255b00c49543253f35c00be
velomad <48247308+velomad@users.noreply.github.com>
2020-12-06 14:46:30 +0530

    caching method changed

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      finally it man, it works
-    </div>
-  );
+	return (
+		<div className="App">
+			finally it man, it works
+			<div
+      style={{backgroundColor:"firebrick"}}
+      >new update</div>
+		</div>
+	);
 }
 
 export default App;
