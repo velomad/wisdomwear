262a923ac630f7c57fbdb17ca2d4dba862bbcfa8
velomad <48247308+velomad@users.noreply.github.com>
2020-12-06 13:26:25 +0530

    firs commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "..", "client-app/build")));
+	app.get("/*", (req, res) => {
+		res.sendFile(path.join(__dirname, "..", "client-app/build", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
+
