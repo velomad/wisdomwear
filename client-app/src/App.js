5a03934c59a7b84ef57bf464755f229be335c478
velomad <48247308+velomad@users.noreply.github.com>
2020-12-07 13:43:51 +0530

    adding tailwind config

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "./styles/app.css";
 
 function App() {
 	return (
 		<div className="App">
-			finally it man, it works
-			<div
-      style={{backgroundColor:"red"}}
-      >new update</div>
-      <p>asda</p>
+			<p className="text-red-500">
+				hello
+			</p>
 		</div>
 	);
 }
